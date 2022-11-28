e215e94f052d1052991f8cc521610456b27d6a56
XiWang <vivian.xi.wang@gmail.com>
2022-11-28 14:23:08 -0800

    Finished curry

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -6,12 +6,40 @@ function sum(){
 function sum2(...args){
     return args.reduce((acc, ele)=> acc+ele)
 }
-console.log(sum(1,2,3,4))
-console.log(sum2(1,2,3,4))
+
 
 Function.prototype.myBind = function(context, ...bindArgs){
     let that = this
     return function(...callArgs){
         return that.apply(context,[...bindArgs,...callArgs])
     }
+}
+
+
+function curriedSum(numArgs) {
+    let nums =[];
+    return function _curriedSum(ele){
+        nums.push(ele);
+
+        if(nums.length < numArgs){
+            return _curriedSum
+        }else{
+            return nums.reduce((acc, ele) => acc + ele)
+        }
+    }
+}
+
+
+
+Function.prototype.curry = function(numArgs){
+        let that = this;
+        let num = [];
+        return function _curry(ele){
+            num.push(ele);
+            if (num.length < numArgs){
+                return _curry;
+            }else{
+                return that(...num)
+            }
+        }
 }
